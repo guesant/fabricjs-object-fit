f4cc2b58389da8c19ece06ecefd793279bae0285
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2026-03-29 19:37:16 -0400

    chore: upgrade rollup

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -3,7 +3,7 @@ import { nodeResolve } from "@rollup/plugin-node-resolve";
 import typescript from "@rollup/plugin-typescript";
 import { defineConfig } from "rollup";
 import dts from "rollup-plugin-dts";
-import { terser } from "rollup-plugin-terser";
+import terser from "@rollup/plugin-terser";
 
 const sharedPluginsBefore = [nodeResolve()];
 const distMinifyPlugins = [terser()];
