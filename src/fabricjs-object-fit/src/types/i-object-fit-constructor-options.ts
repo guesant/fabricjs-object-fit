c0ccae0e3695a4b6bdb1cbaa7ee288873ac4883d
Gabriel R. Antunes <gabrielrodantunes.dev@gmail.com>
2026-03-30 14:55:06 -0400

    fix: reorder import statements in i-object-fit-constructor-options.ts

--- src/fabricjs-object-fit/src/types/i-object-fit-constructor-options.ts
+++ src/fabricjs-object-fit/src/types/i-object-fit-constructor-options.ts
@@ -1,5 +1,5 @@
-import type { IObjectFit } from "./i-object-fit";
 import type { IGetFittedObjectOptions } from "./i-get-fitted-object-options";
+import type { IObjectFit } from "./i-object-fit";
 
 export type IObjectFitConstructorOptions = Partial<
   IGetFittedObjectOptions &
